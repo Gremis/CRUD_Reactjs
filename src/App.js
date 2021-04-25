cdeed3f50e7f9093f0a277508139f7071dd670b7
gremis <18993990tovargremis@gmail.com>
2021-04-25 19:52:37 -0300

    project configuration

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <p className="btn-success btn"></p>
+      
+    </div>
+  );
+}
+
+export default App;
